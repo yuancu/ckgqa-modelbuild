e7c369959e6fc68b81df3eef0cf88d85ac50f0c7
Yuanchun Shen <yuanchu@amazon.com>
2021-08-25 06:03:30 +0000

    Add train script.

---
+++ pipelines/kg-gen/train.py
@@ -0,0 +1,200 @@
+import logging
+import argparse
+import os
+import pathlib
+import json
+from datetime import datetime
+
+import numpy as np
+import boto3
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from model import SubjectModel, ObjectModel
+from dataset import TrainDataset, DevDataset, train_collate_fn, dev_collate_fn
+from utils import para_eval
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+
+    # hyperparameters sent by the client are passed as command-line arguments to the script.
+    parser.add_argument('--epochs', type=int, default=500)
+    parser.add_argument('--batch-size', type=int, default=64)
+    parser.add_argument('--learning-rate', type=float, default=0.005)
+    parser.add_argument('--use-cuda', type=bool, default=False)
+
+    # Data, model, and output directories
+    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    parser.add_argument('--test', type=str, default=os.environ['SM_CHANNEL_TEST'])
+    parser.add_argument('--schema', type=str)
+    
+    # Model-specific parameters
+    parser.add_argument('--bert-model-name', type=str, default='bert-base-chinese')
+    parser.add_argument('--bert-dict-len', type=int, default=21127)
+    parser.add_argument('--word-emb', type=int, default=128)
+    parser.add_argument('--max-sent-len', type=int, default=128) # around 2% of the sentences would be truncated for sent_len of 128
+    
+    # Logs
+    parser.add_argument('--logname', type=str, default='')
+
+    return parser.parse_known_args()
+
+
+def get_train_loader(args):
+    train_data = json.load(open(f"{args.train}/train.json"))
+    train_data = train_data[:2]
+    _, predicate2id = json.load(open(f"{args.schema}/schema.json"))
+    train_dataset = TrainDataset(train_data, args.bert_model_name, args.max_sent_len, predicate2id)
+    train_loader = DataLoader(
+        dataset=train_dataset,      # torch TensorDataset format
+        batch_size=args.batch_size,      # mini batch size
+        shuffle=True,               # random shuffle for training
+        num_workers=1,
+        collate_fn=train_collate_fn,      # subprocesses for loading data
+    )
+    return train_loader
+
+
+def get_test_loader(args):
+    test_data = json.load(open(f"{args.test}/dev.json"))
+    test_data = test_data[:2]
+    test_dataset = DevDataset(test_data, args.bert_model_name, args.max_sent_len)
+    test_loader = DataLoader(
+        dataset=test_dataset,      # torch TensorDataset format
+        batch_size=args.batch_size,      # mini batch size
+        shuffle=True,               # random shuffle for training
+        num_workers=1,
+        collate_fn=dev_collate_fn,      # subprocesses for loading data
+        multiprocessing_context='spawn',
+    )
+    return test_loader
+
+
+def get_model(args):
+    _, predicate2id = json.load(open(f"{args.schema}/schema.json"))
+    num_class = len(predicate2id)
+    subject_model = SubjectModel(args.bert_dict_len, args.word_emb)
+    object_model = ObjectModel(args.word_emb, num_class)
+    return subject_model, object_model
+
+
+def get_tb_writer(args):
+    now = datetime.now()
+    dt_string = now.strftime("%m_%d_%H_%M")
+    if args.logname == '':
+        log_dir = os.path.join(args.output_data_dir, 'tb', dt_string)
+    else:
+        log_dir = os.path.join(args.output_data_dir, 'tb', args.logname + '_' + dt_string)
+    writer = SummaryWriter(log_dir=log_dir)
+    logger.info(f"Tensorboard logs are saved at {log_dir}")
+    return writer
+
+
+def train(subject_model, object_model, train_loader, optimizer, epoch, device=torch.device('cpu'), writer=None, log_interval=10):
+    subject_model.train()
+    object_model.train()
+    train_tqdm = tqdm(enumerate(train_loader), desc="Train")
+    for step, batch in train_tqdm:
+        token_ids, attention_masks, subject_ids, subject_labels, object_labels = batch
+        token_ids, attention_masks, subject_ids, subject_labels, object_labels = \
+            token_ids.to(device), attention_masks.to(device), subject_ids.to(device), \
+            subject_labels.to(device), object_labels.to(device)
+        # predict
+        subject_preds, hidden_states = subject_model(token_ids, attention_mask=attention_masks)
+        object_preds = object_model(hidden_states, subject_ids, attention_masks)
+        # calc loss
+        subject_loss = F.binary_cross_entropy(subject_preds, subject_labels, reduction='none') # (bsz, sent_len)
+        attention_masks = attention_masks.unsqueeze(dim=2)
+        subject_loss = torch.sum(subject_loss * attention_masks) / torch.sum(attention_masks) # ()
+        object_loss = F.binary_cross_entropy(object_preds, object_labels, reduction='none') # (bsz, sent_len, n_classes, 2)
+        object_loss = torch.mean(object_loss, dim=2) # (bsz, sent_len, 2)
+        object_loss = torch.sum(object_loss * attention_masks) / torch.sum(attention_masks) # ()
+        loss_sum = subject_loss + object_loss * 10
+        train_tqdm.set_postfix(loss=loss_sum.item())
+        #updates
+        optimizer.zero_grad()
+        loss_sum.backward()
+        optimizer.step()
+
+        with torch.no_grad():
+            exists_subject = subject_labels.sum().item()
+            correct_subject = torch.logical_and(subject_preds > 0.6, subject_labels > 0.6).sum().item()
+            exists_object = object_labels.sum().item()
+            correct_object = torch.logical_and(object_preds > 0.5, object_labels > 0.5).sum().item()
+
+            if step % log_interval == 0:
+                logger.info(f"epoch {epoch}, step: {step}, loss: {loss_sum.item()}, subject_recall: {correct_subject}/{exists_subject}, object_recall: {correct_object}/{exists_object}")
+                if writer:
+                    writer.add_scalar('train/loss', loss_sum.item(), step + epoch * len(train_loader))
+                    writer.add_scalar('train/loss_subject', subject_loss.item(), step + epoch * len(train_loader))
+                    writer.add_scalar('train/loss_object', object_loss.item(), step + epoch * len(train_loader))
+                    writer.add_scalar('train/recall_subject', correct_subject/exists_subject, step + epoch * len(train_loader))
+                    writer.add_scalar('train/recall_object', correct_object/exists_object, step + epoch * len(train_loader))
+
+
+def evaluate(subject_model, object_model, loader, id2predicate, epoch, writer=None, device=torch.device('cpu')):
+    subject_model.eval()
+    object_model.eval()
+    f1, precision, recall = para_eval(subject_model, object_model, loader, id2predicate, device=device, epoch=epoch, writer=writer)
+    print(f"Eval epoch {epoch}: f1: {f1}, precision: {precision}, recall: {recall}")
+    if writer:
+        writer.add_scalar('eval/f1', f1, epoch)
+        writer.add_scalar('eval/precision', precision, epoch)
+        writer.add_scalar('eval/recall', recall, epoch)
+    return f1, precision, recall
+                    
+
+if __name__ == "__main__":
+    '''
+    invoke:
+    python train.py --output-data-dir ./output --output-model ./output  --train /opt/ml/processing/ie/data/processed --test /opt/ml/processing/ie/data/processed --schema /opt/ml/processing/ie/data/processed
+    '''
+
+    args, _ = parse_args()
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    writer = get_tb_writer(args)
+
+    logger.info('Training data location: {}'.format(args.train))
+    logger.info('Test data location: {}'.format(args.test))
+    train_loader = get_train_loader(args)
+    test_loader = get_test_loader(args)
+
+    batch_size = args.batch_size
+    epochs = args.epochs
+    learning_rate = args.learning_rate
+    logger.info('batch_size = {}, epochs = {}, learning rate = {}'.format(batch_size, epochs, learning_rate))
+
+
+    subject_model, object_model = get_model(args)
+    subject_model = subject_model.to(device)
+    object_model = object_model.to(device)
+    
+    params = subject_model.parameters()
+    params = list(params) + list(object_model.parameters())
+    optimizer = torch.optim.Adam(params, lr=args.learning_rate)
+    
+    id2predicate, _ = json.load(open(f"{args.schema}/schema.json"))
+    
+    for e in range(args.epochs):
+        train(subject_model, object_model, train_loader, optimizer, e, device=device, writer=writer, log_interval=10)
+        if e > 100 and e % 5 == 0:
+            # save model
+            torch.save(subject_model.state_dict(), f"{args.model_dir}/subject_{args.logname}_{e}.pt")
+            torch.save(object_model.state_dict(), f"{args.model_dir}/object_{args.logname}_{e}.pt")      
+        # evaluate on test set
+        evaluate(subject_model, object_model, test_loader, id2predicate, e, writer, device=device)
+
+    

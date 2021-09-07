c6022f9059e21917349919026177167a59454be7
Yuanchun Shen <yuanchu@amazon.com>
2021-09-07 06:28:38 +0000

    Add inference script for sagemaker inference

---
+++ pipelines/question_answering/inference.py
@@ -0,0 +1,128 @@
+import os
+import json
+import sys
+import logging
+from argparse import Namespace
+
+import pandas as pd
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+from predict import get_args, load_model, read_input_file, predict_helper, convert_input_file_to_tensor_dataset
+from utils import load_tokenizer, get_intent_labels, get_slot_labels
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+cwd = os.getcwd()
+logger.info(f"Current file path: {dir_path}")
+logger.info(f"files under current file path: {os.listdir(dir_path)}")
+logger.info(f"Working directory: {cwd}")
+logger.info(f"files under working directory: {os.listdir(cwd)}")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+###################################
+### SAGEMKAER LOAD MODEL FUNCTION
+###################################
+
+def model_fn(model_dir):
+    logger.info(f"model dir is {model_dir}")
+    logger.info(f"files under model dir: {os.listdir(model_dir)}")
+    for file in os.listdir(model_dir):
+        if os.path.isdir(file):
+            logger.info(f"files under {file}: {os.listdir(file)}")
+    
+    pred_config = Namespace(
+        model_dir=model_dir
+    )
+    
+    args = get_args(pred_config)
+    model = load_model(pred_config, args, device)
+    
+    intent_label_lst = get_intent_labels(args)
+    slot_label_lst = get_slot_labels(args)
+    
+    if torch.__version__ == '1.5.1':
+        import torcheia
+        model = model.eval()
+        # attach_eia() is introduced in PyTorch Elastic Inference 1.5.1,
+        model = torcheia.jit.attach_eia(model, 0)
+    return (model, args, intent_label_lst, slot_label_lst)
+
+
+###################################
+### SAGEMKAER PREDICT FUNCTION
+###################################
+
+def predict_fn(input_data, model):
+    '''
+    Args:
+    input_data (list(dict)): list of text and spoes pairs [{'text':, 'spo_list':},]
+    model (tuple): subject model and object model
+    
+    Returns:
+    rel_df (pd.Dataframe): a panda data frame that saves (subject, predicate, object) pairs
+    '''
+    
+    pred_config = Namespace(
+        batch_size=64
+    ) # actually not used in convert_input_file_to_tensor_dataset
+    model, args, intent_label_lst, slot_label_lst = model
+    pad_token_label_id = args.ignore_index
+    tokenizer = load_tokenizer(args)
+    dataset = convert_input_file_to_tensor_dataset(input_data, pred_config, args, tokenizer, pad_token_label_id)
+    slot_preds_list, intent_preds = predict_helper(model, dataset, pred_config, args, device, slot_label_lst, pad_token_label_id)
+
+    logger.info("Prediction Done!")
+    
+    intent_preds_text = [intent_label_lst[intent_pred] for intent_pred in intent_preds]
+    
+    predict_result = {
+        'text': input_data,
+        'intentions': intent_preds_text,
+        'slot_labels': slot_preds_list
+    }
+    
+    
+    
+    print('intention[0] type', type(intent_preds[0]))
+    print('slot_labels[0][0] type', type(slot_preds_list[0][0]))
+
+    return predict_result
+
+
+###################################
+### SAGEMKAER MODEL INPUT FUNCTION
+###################################
+
+def input_fn(serialized_input_data, content_type="text/csv"):
+    lines = []
+    for line in serialized_input_data:
+        line = line.strip()
+        # words = line.split()
+        words = list(line)
+        lines.append(words)
+    logger.info(f"input data length: {len(lines)}")
+    if len(lines) > 1:
+        logger.info(f"first piece of data: {lines[0]}")
+    return lines
+
+
+###################################
+### SAGEMKAER MODEL OUTPUT FUNCTION
+###################################
+
+def output_fn(prediction_output, accept="application/json"):
+    return json.dumps(prediction_output, ensure_ascii=False), accept
+
+
+# test inference code locally
+if __name__ == '__main__':
+    model = model_fn('outputs')
+    with open('processed/test/seq.in') as f:
+        lines = input_fn(f, content_type="text/csv")
+    predict_result = predict_fn(lines, model)
+    json_out = output_fn(predict_result, accept="application/json")
